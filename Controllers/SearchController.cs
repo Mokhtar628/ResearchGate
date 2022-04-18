48a1d3114ea029dd39a61e099491996cdd140fdc
Ahmed <aamr6441@gmail.com>
2022-04-18 18:53:35 +0200

    backend

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ResearchGate.Models;
+namespace ResearchGate.Controllers
+{
+    public class SearchController : Controller
+    {
+        ResearchGateDatabaseEntities db = new ResearchGateDatabaseEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OUT(string searching)
+        {
+
+            return View(db.Authors.Where(x => x.Fname.Contains(searching)).ToList());
+        }
+    }
+}
